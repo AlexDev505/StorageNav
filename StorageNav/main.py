0c4e47f1ed91af87d213adaedad05416d4423f7f
AlexDev505 <aleks.filiov@yandex.ru>
2024-02-21 20:53:59 +0700

    hotfix: main block not execute when app ran in pythonanywhere

--- StorageNav/main.py
+++ StorageNav/main.py
@@ -5,11 +5,12 @@ import web_app  # noqa
 from app import app, config
 
 
-if __name__ == "__main__":
-    logger.info("app started")
+if config.base.run_web_app:
+    tg.setup_webhook()
+logger.info("app started")
 
+if __name__ == "__main__":
     if config.base.run_web_app:
-        tg.setup_webhook()
         app.run(debug=True)
     else:
         tg.run_pooling()
