df56122022ed7a5ed456ac21a6af9dc528190370
AlexDev505 <aleks.filiov@yandex.ru>
2024-02-27 13:32:00 +0700

    scaling map

--- StorageNav/web_app/static/js/index.js
+++ StorageNav/web_app/static/js/index.js
@@ -20,20 +20,101 @@ Telegram.WebApp.onEvent('backButtonClicked', function() {
     sendRequest("/logout", {}, none)
 })
 
-sendRequest(
-    "/login",
-    {"hash_string": window.Telegram.WebApp.initDataUnsafe.hash, "data": window.Telegram.WebApp.initData},
-    function(resp) {
-        console.log(resp)
-        document.getElementById("test").style = ""
-        document.getElementById("test_1").innerHTML = JSON.stringify(resp)
-        document.querySelector(".loading-block").style = "display: none"
-        if (resp.status == "ok") {
-            sendRequest(
-                "/test", {}, function(resp) {
-                    document.getElementById("test_2").innerHTML = JSON.stringify(resp)
-                }
-            )
-        }
+hideLoadingBlock()
+showMainBlock()
+
+//sendRequest(
+//    "/login",
+//    {"hash_string": window.Telegram.WebApp.initDataUnsafe.hash, "data": window.Telegram.WebApp.initData},
+//    function(resp) {
+//        hideLoadingBlock()
+//        if (resp.status == "ok") showMainBlock()
+//        else showCloseAppBtnBlock(resp.message + "\ntry again latter...")
+//    }
+//)
+
+function hideLoadingBlock() {
+    document.getElementById("loading-block").style = "display: none"
+}
+function showMainBlock() {
+    document.getElementById("main-block").style = ""
+}
+function showCloseAppBtnBlock(message) {
+    document.getElementById("close-app-btn-block").style = ""
+    document.getElementById("close-message").innerHTML = message
+}
+
+
+let map_scale = 1
+function scaleUpMap() {
+    if (map_scale > 1.9) return
+    map_scale += 0.1
+    applyScaleMap(1)
+}
+function scaleLowMap() {
+    if (map_scale < 0.6) return
+    map_scale -= 0.1
+    applyScaleMap(-1)
+}
+function applyScaleMap(delta) {
+    map = document.getElementById("map")
+    map_block = document.getElementById("map-block")
+    if (map_scale > 1 || (map_scale == 1 & delta == -1)) {
+        map.style = `transform: scale(${map_scale});
+            top: ${1000 * (map_scale - 1) / 2}px;
+            left: ${1000 * (map_scale - 1) / 2}px`
+        map_block.scroll({
+          top: map_block.scrollTop + 25 * delta,
+          left: map_block.scrollLeft + 25 * delta,
+        });
+    } else {
+        map.style = `transform: scale(${map_scale});
+            top: ${1000 * Math.abs(map_scale - 1) / 4}px;
+            left: ${1000 * Math.abs(map_scale - 1) / 4}px`
+        map_block.scroll({
+          top: map_block.scrollTop - 75 * delta,
+          left: map_block.scrollLeft - 75 * delta,
+        });
     }
-)
+}
+
+
+//let handle_move = false
+//let last_touches = []
+//let last_dist = 0
+//let map_scale = 1
+//document.getElementById("map").addEventListener('touchstart', function(e) {
+//    handle_move = (e.touches.length == 2)
+//    if (handle_move) {
+//        last_touches = [
+//            [e.touches[0].pageX, e.touches[0].pageY],
+//            [e.touches[1].pageX, e.touches[1].pageY]
+//        ]
+//        last_dist = Math.hypot(
+//            e.touches[0].pageX - e.touches[1].pageX,
+//            e.touches[0].pageY - e.touches[1].pageY
+//        )
+//        document.getElementById("test").innerHTML = `start`
+//    }
+//}, false);
+//document.getElementById("map").addEventListener('touchend', function(e) {
+//    handle_move = (e.touches.length == 2)
+//}, false);
+//document.getElementById("map").addEventListener('touchmove', function(e) {
+//    if (!handle_move) return
+//    document.getElementById("test").innerHTML = `move`
+//    last_touches = [
+//        [e.touches[0].pageX, e.touches[0].pageY],
+//        [e.touches[1].pageX, e.touches[1].pageY]
+//    ]
+//    dist = Math.hypot(
+//        e.touches[0].pageX - e.touches[1].pageX,
+//        e.touches[0].pageY - e.touches[1].pageY
+//    )
+//    if (dist < last_dist) {
+//        map_scale -= 0.05
+//    } else {
+//        map_scale += 0.05
+//    }
+//    last_dist = dist
+//}, false);
