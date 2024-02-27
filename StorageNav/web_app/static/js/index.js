e78dce34e8da16b49127abc1efab36e6a1c73464
AlexDev505 <aleks.filiov@yandex.ru>
2024-02-27 13:59:43 +0700

    scaling map by pinch

--- StorageNav/web_app/static/js/index.js
+++ StorageNav/web_app/static/js/index.js
@@ -48,73 +48,65 @@ function showCloseAppBtnBlock(message) {
 let map_scale = 1
 function scaleUpMap() {
     if (map_scale > 1.9) return
-    map_scale += 0.1
-    applyScaleMap(1)
+    applyScaleMap(0.1)
 }
 function scaleLowMap() {
     if (map_scale < 0.6) return
-    map_scale -= 0.1
-    applyScaleMap(-1)
+    applyScaleMap(-0.1)
 }
 function applyScaleMap(delta) {
+    if (map_scale + delta > 2 || map_scale + delta < 0.5) return
+    map_scale += delta
+    delta_sign = delta / Math.abs(delta)
+
     map = document.getElementById("map")
     map_block = document.getElementById("map-block")
-    if (map_scale > 1 || (map_scale == 1 & delta == -1)) {
+    if (map_scale > 1 || (map_scale == 1 & delta < 0)) {
         map.style = `transform: scale(${map_scale});
             top: ${1000 * (map_scale - 1) / 2}px;
             left: ${1000 * (map_scale - 1) / 2}px`
         map_block.scroll({
-          top: map_block.scrollTop + 25 * delta,
-          left: map_block.scrollLeft + 25 * delta,
+          top: map_block.scrollTop + (1000 * delta / 4),
+          left: map_block.scrollLeft + (1000 * delta / 4),
         });
     } else {
         map.style = `transform: scale(${map_scale});
             top: ${1000 * Math.abs(map_scale - 1) / 4}px;
             left: ${1000 * Math.abs(map_scale - 1) / 4}px`
         map_block.scroll({
-          top: map_block.scrollTop - 75 * delta,
-          left: map_block.scrollLeft - 75 * delta,
+          top: map_block.scrollTop - (1000 * delta / 4 * 3),
+          left: map_block.scrollLeft - (1000 * delta / 4 * 3),
         });
     }
 }
 
 
-//let handle_move = false
-//let last_touches = []
-//let last_dist = 0
-//let map_scale = 1
-//document.getElementById("map").addEventListener('touchstart', function(e) {
-//    handle_move = (e.touches.length == 2)
-//    if (handle_move) {
-//        last_touches = [
-//            [e.touches[0].pageX, e.touches[0].pageY],
-//            [e.touches[1].pageX, e.touches[1].pageY]
-//        ]
-//        last_dist = Math.hypot(
-//            e.touches[0].pageX - e.touches[1].pageX,
-//            e.touches[0].pageY - e.touches[1].pageY
-//        )
-//        document.getElementById("test").innerHTML = `start`
-//    }
-//}, false);
-//document.getElementById("map").addEventListener('touchend', function(e) {
-//    handle_move = (e.touches.length == 2)
-//}, false);
-//document.getElementById("map").addEventListener('touchmove', function(e) {
-//    if (!handle_move) return
-//    document.getElementById("test").innerHTML = `move`
-//    last_touches = [
-//        [e.touches[0].pageX, e.touches[0].pageY],
-//        [e.touches[1].pageX, e.touches[1].pageY]
-//    ]
-//    dist = Math.hypot(
-//        e.touches[0].pageX - e.touches[1].pageX,
-//        e.touches[0].pageY - e.touches[1].pageY
-//    )
-//    if (dist < last_dist) {
-//        map_scale -= 0.05
-//    } else {
-//        map_scale += 0.05
-//    }
-//    last_dist = dist
-//}, false);
+let handle_move = false
+let last_dist = 0
+document.getElementById("map").addEventListener('touchstart', function(e) {
+    handle_move = (e.touches.length == 2)
+    if (handle_move) {
+        last_dist = Math.hypot(
+            e.touches[0].pageX - e.touches[1].pageX,
+            e.touches[0].pageY - e.touches[1].pageY
+        )
+        document.getElementById("map-block").style = "overflow: hidden;"
+    }
+}, false);
+document.getElementById("map").addEventListener('touchend', function(e) {
+    handle_move = false
+    document.getElementById("map-block").style = ""
+}, false);
+document.getElementById("map").addEventListener('touchmove', function(e) {
+    if (!handle_move) return
+    dist = Math.hypot(
+        e.touches[0].pageX - e.touches[1].pageX,
+        e.touches[0].pageY - e.touches[1].pageY
+    )
+    if (dist < last_dist) {
+        applyScaleMap(-0.01)
+    } else {
+        applyScaleMap(0.01)
+    }
+    last_dist = dist
+}, false);
